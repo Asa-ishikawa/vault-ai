d93955cdaecc1bc61c5ab32bf1b5c34b36d2fe81
Asa-ishikawa <ishikawa.anry@gmail.com>
2026-07-28 23:36:59 +0000

    STEP4 pose前半

--- pose.js
+++ pose.js
@@ -1,30 +1,112 @@
 // =========================
-// 跳び箱AI Pose
-// Version1
+// MediaPipe Pose
 // =========================
 
+let pose;
+
+// 初期化
 function startPose(video, canvas, ctx){
+
+    const status = document.getElementById("status");
+
+    status.textContent = "AIを初期化しています...";
+
+    pose = new Pose({
+
+        locateFile: (file) => {
+
+            return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
+
+        }
+
+    });
+
+    pose.setOptions({
+
+        modelComplexity:1,
+
+        smoothLandmarks:true,
+
+        enableSegmentation:false,
+
+        smoothSegmentation:false,
+
+        minDetectionConfidence:0.5,
+
+        minTrackingConfidence:0.5
+
+    });
+
+    pose.onResults((results)=>{
+
+        drawPose(results,canvas,ctx);
+
+    });
+
+    analyzeVideo(video);
+
+}
+// =========================
+// 動画解析
+// =========================
+
+async function analyzeVideo(video){
+
+    while(true){
+
+        if(video.paused || video.ended){
+
+            await new Promise(r=>setTimeout(r,30));
+
+            continue;
+
+        }
+
+        await pose.send({
+
+            image:video
+
+        });
+
+    }
+
+}
+// =========================
+// 骨格描画
+// =========================
+
+function drawPose(results,canvas,ctx){
 
     ctx.clearRect(0,0,canvas.width,canvas.height);
 
-    ctx.fillStyle="lime";
+    if(!results.poseLandmarks){
 
-    ctx.beginPath();
+        return;
 
-    ctx.arc(
+    }
 
-        canvas.width/2,
+    for(const landmark of results.poseLandmarks){
 
-        canvas.height/2,
+        ctx.beginPath();
 
-        40,
+        ctx.arc(
 
-        0,
+            landmark.x*canvas.width,
 
-        Math.PI*2
+            landmark.y*canvas.height,
 
-    );
+            5,
 
-    ctx.fill();
+            0,
+
+            Math.PI*2
+
+        );
+
+        ctx.fillStyle="red";
+
+        ctx.fill();
+
+    }
 
 }
