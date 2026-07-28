b90251e0faad8afec26aece0e6a8ceda4f09db02
Asa-ishikawa <ishikawa.anry@gmail.com>
2026-07-28 00:14:09 +0000

    動画とCanvas追加

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
-const videoFile=document.getElementById("videoFile");
-const video=document.getElementById("video");
+const videoFile = document.getElementById("videoFile");
+const video = document.getElementById("video");
+const canvas = document.getElementById("outputCanvas");
+const ctx = canvas.getContext("2d");
 
-videoFile.addEventListener("change",()=>{
+videoFile.addEventListener("change", () => {
 
-    const file=videoFile.files[0];
+    const file = videoFile.files[0];
 
     if(!file) return;
 
-    const url=URL.createObjectURL(file);
+    const url = URL.createObjectURL(file);
 
-    video.src=url;
+    video.src = url;
+
+});
+
+video.addEventListener("loadedmetadata",()=>{
+
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
 
 });
 
 document.getElementById("detectBtn").addEventListener("click",()=>{
 
-   startPose(); 
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    ctx.fillStyle="red";
+
+    ctx.beginPath();
+
+    ctx.arc(
+        canvas.width/2,
+        canvas.height/2,
+        15,
+        0,
+        Math.PI*2
+    );
+
+    ctx.fill();
 
 });
